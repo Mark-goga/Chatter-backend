f2162c69e71c251cab7bde0866527f75b98130a4
Mark-Goga <markgala06@gmail.com>
2025-02-11 13:37:32 +0200

    small fix injecteble

--- src/chats/chats.repository.ts
+++ src/chats/chats.repository.ts
@@ -3,13 +3,14 @@ import {AbstractRepository} from "../common/database/abstract.repository";
 import {InjectModel} from "@nestjs/mongoose";
 import {Model} from "mongoose";
 import {ChatDocument} from "./entities/chat.document";
+import {Chat} from "./entities/chat.entity";
 
 @Injectable()
 export class ChatsRepository extends AbstractRepository<ChatDocument> {
 
 	protected readonly logger = new Logger(ChatsRepository.name);
 
-	constructor(@InjectModel(ChatDocument.name) chatsModel: Model<ChatDocument>) {
+	constructor(@InjectModel(Chat.name) chatsModel: Model<ChatDocument>) {
 		super(chatsModel);
 	}
 }
